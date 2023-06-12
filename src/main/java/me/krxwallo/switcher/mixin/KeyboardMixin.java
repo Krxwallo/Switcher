a834744ce565759159916384bf9c7686fb1896a8
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-06-12 18:17:20 +0200

    1.1.0: update to 1.20.1 + add support for F3+N

--- src/main/java/me/krxwallo/switcher/mixin/KeyboardMixin.java
+++ src/main/java/me/krxwallo/switcher/mixin/KeyboardMixin.java
@@ -1,20 +1,49 @@
 package me.krxwallo.switcher.mixin;
 
+import com.google.common.base.MoreObjects;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.GameModeSelectionScreen;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.world.GameMode;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Keyboard.class)
 public class KeyboardMixin {
+	@Shadow @Final private MinecraftClient client;
+
+	/**
+	 * Injection to always:
+	 * - open the game mode selection screen when F3+F4 is pressed
+	 * - or switch the game mode accordingly when F3+N is pressed,
+	 * thereby bypassing the permission check when opening.
+	 * @param key the key that was pressed with F3
+	 */
 	@Inject(at = @At("HEAD"), method = "processF3", cancellable = true)
 	private void processF3(int key, CallbackInfoReturnable<Boolean> cir) {
-		if (key == 293) {
-			MinecraftClient.getInstance().setScreen(new GameModeSelectionScreen());
-			cir.setReturnValue(true);
+		switch (key) {
+			case 293 -> { // F4
+				MinecraftClient.getInstance().setScreen(new GameModeSelectionScreen());
+				cir.setReturnValue(true);
+			}
+			case 78 -> { // N
+				if (client.player == null) return;
+				if (client.interactionManager == null) return;
+				if (!this.client.player.isSpectator())
+					this.client.player.networkHandler.sendCommand("gamemode spectator");
+				else {
+					ClientPlayNetworkHandler networkHandler = this.client.player.networkHandler;
+					GameMode previousGameMode = this.client.interactionManager.getPreviousGameMode();
+					networkHandler.sendCommand("gamemode " + MoreObjects.firstNonNull(previousGameMode, GameMode.CREATIVE).getName());
+				}
+				cir.setReturnValue(true);
+			}
 		}
+
 	}
 }
--- src/main/java/me/krxwallo/switcher/mixin/SwitcherScreenMixin.java
+++ src/main/java/me/krxwallo/switcher/mixin/SwitcherScreenMixin.java
@@ -8,8 +8,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameModeSelectionScreen.class)
 public class SwitcherScreenMixin {
-    @Redirect(method = "apply(Lnet/minecraft/client/MinecraftClient;Ljava/util/Optional;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z"))
-    private static boolean apply(ClientPlayerEntity player, int level) {
+    /**
+     * Redirect the `hasPermissionLevel(2) call to always return true, so that the game mode command is always sent.`
+     * @param player our client player
+     * @param level the permission level to check/ignore
+     * @return true
+     */
+    @Redirect(method = "apply(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/GameModeSelectionScreen$GameModeSelection;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z"))
+    private static boolean ignorePermissionLevel(ClientPlayerEntity player, int level) {
         return true; // Always send the command, even without the permission level
     }
 }
